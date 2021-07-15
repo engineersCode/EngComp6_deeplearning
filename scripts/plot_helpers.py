d6117686a789293eeeb4b87e6545a2feae01374e
Tingyu Wang <tingyu.wang6@gmail.com>
2021-07-15 01:02:35 -0400

    first pass to lesson 4, hide plotting helpers to scripts

---
+++ scripts/plot_helpers.py
@@ -0,0 +1,44 @@
+import numpy as _np
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+from matplotlib import pyplot as plt
+from ipywidgets import interact
+
+def interact_polyreg(max_degree, x, y, regularized=False):
+    """
+    The function to plot polynomial linear regression.
+    
+    Args:
+        max_degree: Max polynomial degree.
+        x,y: 1D Training data.
+        regularized: Whether to add l2-norm regularization term in loss. Default to False.
+    """
+    x_plot = _np.linspace(x.min(), x.max(), 30).reshape(-1,1)
+
+    def polyreg_helper(degree):
+        plt.figure(figsize=(10,6))
+        plt.scatter(x, y, c='r', label='true')
+        linear = make_pipeline(PolynomialFeatures(degree, include_bias=False),
+                               MinMaxScaler(),
+                               LinearRegression(fit_intercept=True))
+        linear.fit(x.reshape(-1,1), y)
+        mae_linear = mean_absolute_error(y, linear.predict(x.reshape(-1,1)))
+
+        if regularized:
+            ridge = make_pipeline(PolynomialFeatures(degree, include_bias=False),
+                                  MinMaxScaler(),
+                                  Ridge(alpha=1.0))
+            ridge.fit(x.reshape(-1,1), y)
+            mae_ridge = mean_absolute_error(y, ridge.predict(x.reshape(-1,1)))
+            plt.plot(x_plot, linear.predict(x_plot), label='predicted, w/o regularization')
+            plt.plot(x_plot, ridge.predict(x_plot), label='predicted, with regularization')
+            plt.title(f"Poly degree = {degree:2}, MAE_no_reg = {mae_linear:.3f}, MAE_reg = {mae_ridge:.3f}", fontsize=16)
+        else:
+            plt.plot(x_plot, linear.predict(x_plot), label='predicted')
+            plt.title(f"Polynomial degree = {degree:2}, MAE = {mae_linear:.3f}", fontsize=16)
+        plt.legend(fontsize=16)
+        plt.show()
+        
+    interact(polyreg_helper, degree=(1, max_degree))
