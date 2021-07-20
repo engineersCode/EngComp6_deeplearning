63f446a744ca7d27dc37dc9ed7b460bdd18a9982
Tingyu Wang <tingyu.wang6@gmail.com>
2021-07-20 11:36:28 -0400

    some edits in lesson 4, print weights in plotting script

--- scripts/plot_helpers.py
+++ scripts/plot_helpers.py
@@ -6,14 +6,19 @@ from sklearn.metrics import mean_absolute_error
 from matplotlib import pyplot as plt
 from ipywidgets import interact
 
-def interact_polyreg(max_degree, x, y, regularized=False):
+def interact_polyreg(max_degree, x, y, regularized=False, verbose=True):
     """
     The function to plot polynomial linear regression.
     
     Args:
-        max_degree: Max polynomial degree.
-        x,y: 1D Training data.
-        regularized: Whether to add l2-norm regularization term in loss. Default to False.
+        max_degree: int
+            Max polynomial degree.
+        x,y: numpy.ndarray
+            1D Training data.
+        regularized: bool
+            Whether to add l2-norm regularization term in loss. Default to False.
+        verbose: bool
+            Whether to print trained weights. Default to True.
     """
     x_plot = _np.linspace(x.min(), x.max(), 30).reshape(-1,1)
 
@@ -35,6 +40,11 @@ def interact_polyreg(max_degree, x, y, regularized=False):
             plt.plot(x_plot, linear.predict(x_plot), label='predicted, w/o regularization')
             plt.plot(x_plot, ridge.predict(x_plot), label='predicted, with regularization')
             plt.title(f"Poly degree = {degree:2}, MAE_no_reg = {mae_linear:.3f}, MAE_reg = {mae_ridge:.3f}", fontsize=16)
+            if verbose:
+                print('weights without regularization')
+                print(linear.named_steps['linearregression'].coef_)
+                print('weights with regularization')
+                print(ridge.named_steps['ridge'].coef_)
         else:
             plt.plot(x_plot, linear.predict(x_plot), label='predicted')
             plt.title(f"Polynomial degree = {degree:2}, MAE = {mae_linear:.3f}", fontsize=16)
